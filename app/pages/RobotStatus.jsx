9dc4e4bb431ea000639ea7a6e9d48a4e87e4cd76
Shilong Sun <shlsun@bainainfo.com>
2017-03-21 09:52:11 +0800

    机器人统计信息

--- app/pages/RobotStatus.jsx
+++ app/pages/RobotStatus.jsx
@@ -11,58 +11,78 @@ class RobotTable extends React.Component {
 
     render() {
         const now = moment().valueOf();
+        let sumCoins = 0;
+        let sumGMCoins = 0;
+        let online = 0;
+        let uniqueTables = [];
         const entries = this.props.list.map((item, index) => 
-            (
-                <tr key={index}>
-                    <td>{item.account_id}</td>
-                    <td>{item.table_id}</td>
-                    <td>{item.nick_name}</td>
-                    <td>{item.coins}</td>
-                    <td>{item.gm_add_coins}</td>
-                    <td>{parseFloat(item.coins) - parseFloat(item.gm_add_coins) - 18888}</td>
-                    <td>{item.weight}</td>
-                    <td>{item.robot_type == 1 ? "Mimics" : "Follower"}</td>
-                    <td>{(function (roboTime, curTime) {
-                        // var botTimeStr = "(" + moment(roboTime).format() + ")";
-                        let botTimeStr = "";
-                        if (roboTime <= curTime) {
-                            return botTimeStr + "Awake";
-                        } else {
-                            let dur = moment.duration(roboTime - curTime);
-                            return botTimeStr + dur.hours() + ":" + dur.minutes() + ":" + dur.seconds();
-                        }
-                    })(Number(item.wake_time) * 1000, now)}</td>
-                    <td>{item.config_id}</td>
-                    <td>{item.exp_level}</td>
-                    <td>{item.add_coin_times}</td>
-                    <td>{item.change_time}</td>
-                </tr>
-            )
+            {
+                sumCoins += parseInt(item.coins);
+                sumGMCoins += parseInt(item.gm_add_coins);
+                online += parseInt(item.table_id) == 0 ? 0 : 1;
+                if (uniqueTables.indexOf(item.table_id) == -1) {
+                    uniqueTables.push(item.table_id);
+                }
+                return (
+                    <tr key={index}>
+                        <td>{item.account_id}</td>
+                        <td>{item.table_id}</td>
+                        <td>{item.nick_name}</td>
+                        <td>{item.coins}</td>
+                        <td>{item.gm_add_coins}</td>
+                        <td>{parseFloat(item.coins) - parseFloat(item.gm_add_coins) - 18888}</td>
+                        <td>{item.weight}</td>
+                        <td>{item.robot_type == 1 ? "Mimics" : "Follower"}</td>
+                        <td>{(function (roboTime, curTime) {
+                            // var botTimeStr = "(" + moment(roboTime).format() + ")";
+                            let botTimeStr = "";
+                            if (roboTime <= curTime) {
+                                return botTimeStr + "Awake";
+                            } else {
+                                let dur = moment.duration(roboTime - curTime);
+                                return botTimeStr + dur.hours() + ":" + dur.minutes() + ":" + dur.seconds();
+                            }
+                        })(Number(item.wake_time) * 1000, now)}</td>
+                        <td>{item.config_id}</td>
+                        <td>{item.exp_level}</td>
+                        <td>{item.add_coin_times}</td>
+                        <td>{item.change_time}</td>
+                    </tr>
+                );
+            }
         );
         return (
-            <div className="table-responsive">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>account_id</th>
-                            <th>table_id</th>
-                            <th>nick_name</th>
-                            <th>coins</th>
-                            <th>gm_add_coins</th>
-                            <th>won</th>
-                            <th>weight</th>
-                            <th>robot_type</th>
-                            <th>wake_time</th>
-                            <th>config_id</th>
-                            <th>exp_level</th>
-                            <th>add_coin_times</th>
-                            <th>change_time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {entries}
-                    </tbody>
-                </table>
+            <div>
+                <div className="row">
+                    <span className="col-xs-3"><label>机器人总数：</label>{`${online} / ${this.props.list.length}`}</span>
+                    <span className="col-xs-3"><label>总筹码：</label>{sumCoins}</span>
+                    <span className="col-xs-3"><label>总赢取：</label>{sumCoins - sumGMCoins - 18888 * this.props.list.length}</span>
+                    <span className="col-xs-3"><label>牌桌数：</label>{uniqueTables.length - 1}</span>
+                </div>
+                <div className="table-responsive">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>account_id</th>
+                                <th>table_id</th>
+                                <th>nick_name</th>
+                                <th>coins</th>
+                                <th>gm_add_coins</th>
+                                <th>won</th>
+                                <th>weight</th>
+                                <th>robot_type</th>
+                                <th>wake_time</th>
+                                <th>config_id</th>
+                                <th>exp_level</th>
+                                <th>add_coin_times</th>
+                                <th>change_time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {entries}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
