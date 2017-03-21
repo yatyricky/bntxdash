8301c380841c8e6ba464a7b66f97c1ba044ef335
Shilong Sun <shlsun@bainainfo.com>
2017-03-21 14:46:09 +0800

    添加图表

--- app/pages/RobotCoinsSum.jsx
+++ app/pages/RobotCoinsSum.jsx
@@ -8,6 +8,7 @@ class RobotCoinsSum extends React.Component {
         super();
         this.postData = this.postData.bind(this);
         this.lastRequest = null;
+        this.config = {categories: [], data: []};
         this.state = {
             "flag": Flag.nothing
         }
@@ -43,10 +44,7 @@ class RobotCoinsSum extends React.Component {
     }
 
     renderTable() {
-        const config = {categories: [], data: []};
         const entries = this.state.result.resp.map((item, index) => {
-            config.categories.push(item.time);
-            config.data.push(item.value);
             return (
                 <tr key={index}>
                     <td>{item.time}</td>
@@ -75,9 +73,31 @@ class RobotCoinsSum extends React.Component {
         let ret;
         switch (flag) {
             case Flag.success:
+                for (let i = 0; i < this.state.result.resp.length; i++) {
+                    this.config.categories.push(this.state.result.resp[i].time.split(" ")[0]);
+                    this.config.data.push(parseInt(this.state.result.resp[i].value));
+                }
+                const highConfig = {
+                    title: {
+                        text: '每日筹码存量'
+                    },
+                    xAxis: {
+                        categories: this.config.categories
+                    },
+                    yAxis: {
+                        title: {
+                            text: '总量'
+                        }
+                    },
+                    series: [{
+                        name: '总筹码',
+                        data: this.config.data
+                    }]
+
+                };
                 ret = (
                     <div>
-                        
+                        <ReactHighcharts config = {highConfig} />
                         <div className="table-responsive">{this.renderTable()}</div>
                     </div>
                 );
@@ -101,7 +121,7 @@ class RobotCoinsSum extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="page-header">机器人最近金币存量</h1>
+                <h1 className="page-header">机器人最近筹码存量</h1>
                 <div>{this.renderResult(this.state.flag)}</div>
             </div>
         );
